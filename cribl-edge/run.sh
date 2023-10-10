bc14e24ad281efeff0ef570f53473f49846cb48e
Skye Lowry <patrickskyelowry@gmail.com>
2023-10-10 10:57:30 -0700

    Update run.sh

--- cribl-edge/run.sh
+++ cribl-edge/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /opt
-curl -Lso - https://cdn.cribl.io/dl/4.3.0/cribl-4.3.0-f9e4a40c-linux-x64.tgz | tar zxv
+#curl -Lso - https://cdn.cribl.io/dl/4.3.0/cribl-4.3.0-f9e4a40c-linux-x64.tgz | tar zxv
 ls -l cribl/bin
 cd cribl/bin
 cribl mode mode-managed-edge -H 192.168.60.115 -p 4200
