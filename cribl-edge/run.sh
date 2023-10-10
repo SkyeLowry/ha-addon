6c610fbc68d0316adf80322e8110551420d25d62
Skye Lowry <patrickskyelowry@gmail.com>
2023-10-10 10:36:03 -0700

    Update run.sh

--- cribl-edge/run.sh
+++ cribl-edge/run.sh
@@ -4,6 +4,7 @@ echo "Your API key is: $HASSIO_TOKEN"
 
 cd /opt
 curl -Lso - https://cdn.cribl.io/dl/4.3.0/cribl-4.3.0-f9e4a40c-linux-x64.tgz | tar zxv
-ls
+ls cribl
+ls cribl/bin
 /opt/cribl/bin/cribl mode mode-managed-edge -H 192.168.60.115 -p 4200
 /opt/cribl/bin/cribl start
